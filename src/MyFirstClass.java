a1ea15e962dab072cd3ad757f51adba68c4f24a9
Yoan15 <yoann.deco@gmail.com>
2023-07-03 17:32:47 +0200

    First Commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,11 @@
+public class MyFirstClass {
+
+    public static void main(String[] args){
+        System.out.println("Hello Everyone !");
+        System.out.println("This is the second instruction that i have executed");
+
+        System.out.print("My name is ");
+        System.out.print("Yoan");
+    }
+
+}
