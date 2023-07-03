bc989c6bd00e16a3387b020069bdd13fec5a1b23
Yoan15 <yoann.deco@gmail.com>
2023-07-03 17:41:30 +0200

    Différents types de données

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -6,6 +6,17 @@ public class MyFirstClass {
 
         System.out.print("My name is ");
         System.out.print("Yoan");
+        System.out.println();
+
+        System.out.println(1);
+        System.out.println(999);
+        System.out.println(2.45);
+        System.out.println(2.45F);
+        System.out.println(99999999999L);
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println('a');
+        System.out.println('%');
     }
 
 }
