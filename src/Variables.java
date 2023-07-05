e02b4d11ee15a074d76588257d1c9fff92668691
Yoan15 <yoann.deco@gmail.com>
2023-07-05 19:00:00 +0200

    Variables test

---
+++ src/Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        String name = "Yoan";
+        System.out.println(name);
+        // I like Java
+        String myText = "I like Java";
+        System.out.println(myText);
+        myText = "New text";
+        System.out.println(myText);
+
+        int myInteger = 100;
+        System.out.println(myInteger);
+
+        double myDoubleNumber = 55.6;
+        System.out.println(myDoubleNumber);
+
+        float myFloatNumber = 44.4F;
+        System.out.println(myFloatNumber);
+
+        char letter = 'a';
+        System.out.println(letter);
+
+        boolean value = true;
+        System.out.println(value);
+    }
+}
